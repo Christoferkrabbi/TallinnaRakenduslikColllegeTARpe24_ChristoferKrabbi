b3f78498c5420c955b62a6306b5c8bbe6e1f7333
christoferkrabbi <christoferkrabbi@gmail.com>
2025-09-29 14:13:16 +0300

    nigga

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikColllegeTARpe24_ChristoferKrabbi.Data;
+using TallinnaRakenduslikColllegeTARpe24_ChristoferKrabbi.Models;
 
 namespace TallinnaRakenduslikColllegeTARpe24_ChristoferKrabbi.Controllers
 {
@@ -13,8 +14,47 @@ namespace TallinnaRakenduslikColllegeTARpe24_ChristoferKrabbi.Controllers
 		}
 		public IActionResult Index()
 		{
-			var courses = _context.Courses.Include(c => c.Department).AsNoTracking();
+			var courses = _context.Courses.Include(c => c.Department)
+				.AsNoTracking();
+            return View(courses);
+        }
+		[HttpGet]
+		public IActionResult Create()
+		{
+			PopulateDepartmentsDropdDownList();
 			return View();
 		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(Course course)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(course);
+				await _context.SaveChangesAsync();
+				PopulateDepartmentsDropDownList(course.DepartmentID);	
+			}
+			return RedirectToAction("Index");
+		}
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Courses == null)
+			{
+				return NotFound();
+			}
+			var course = await _context.Courses.FindAsync(id);
+			if (course != null)
+			{
+				_context.Courses.Remove(course);
+			}
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index");
+			{
+			}
+		}
+
+
 	}
 }
